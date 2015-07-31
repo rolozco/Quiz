64bf2c42f8a32ce6932ca36b12edb15b0bd8b0f1
rolozco <rolozco@ono.com>
2015-07-31 21:18:34 +0200

    Creación de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,3 +39,18 @@ exports.answer = function(req, res) {
   res.render(
     'quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
+//GET /quizes/new
+exports.new = function (req, res){
+  var quiz = models.Quiz.build(
+    {pregunta:"Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz:quiz});
+};
+//POST /quizes/create
+exports.create = function (req, res) {
+  var quiz = models.Quiz.build (req.body.quiz);
+  //guarda en BBDD los campos pregunta y respuesta de quiz
+  quiz.save({fileds: ["pregunta", "respuesta"]}).then (function (){
+    res.redirect('/quizes'); //REdirección HTTP (URL relativo) a lista de preguntas
+  })
+};
