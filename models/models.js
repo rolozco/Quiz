98114b1225cb5098a338459e91823cd6fd3690af
rolozco <rolozco@ono.com>
2015-07-31 19:38:30 +0200

    Autoload

--- models/models.js
+++ models/models.js
@@ -36,8 +36,9 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function() {
 	Quiz.count().then(function(count) {
 		if (count === 0) {
-			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'})
-			.then(function(){console.log('Base de datos inicializada')});
-		};
+      Quiz.create({ pregunta: "Capital de Italia", respuesta: "Roma"});
+      Quiz.create({ pregunta: "Capital de Portugal", respuesta: "Lisboa"})
+      .then(function(){console.log("Base de datos inicializada")});
+    };
 	});
 });
